726f55b4a6d40b24932e9647fce9920b7472ebbb
Carolyn Larocque <clarocque1@live.ca>
2018-02-12 13:45:06 -0500

    Adding database information, and also maybe handlebar stuff if I messed that other branch up

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var mysql = require('mysql');
+var connect = require('../utils/sqlConnect');
 var router = express.Router();
 
 const templateTypes = [
@@ -9,10 +11,19 @@ const templateTypes = [
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Express',
-    message : "I like EJS.",
-    templates: templateTypes
+  connect.query('SELECT * FROM mainmodel', (err, result, fields) => {
+    if(err) {
+      throw err; console.log(err);
+    } else {
+      console.log(result);
+    }
+  });
+
+  res.render('home', {
+    title: 'Handlebars',
+    message : "I like Handlebars.",
+    templates: templateTypes,
+    anothermessage: "handlebars is awesome"
   });
 });
 
